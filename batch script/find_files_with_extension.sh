c62f7caef61a8fc8dabd87ad2236515dff12779c
Amit <90318118+aamit107@users.noreply.github.com>
2023-02-28 12:11:03 +0600

    Create find_files_with_extension.sh

---
+++ batch script/find_files_with_extension.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function find_files_with_extension() {
+    local dir_to_search="$1"
+    local extension="$2"
+    local file_names=()
+
+    for file_name in "$dir_to_search"/*".$extension"; do
+        if [ -f "$file_name" ]; then  # Check if the file exists and is a regular file
+            file_names+=("$file_name")
+        fi
+    done
+    echo "${file_names[@]}"
+}
+
+echo "Type name of the folder:"
+read -r dir_to_search
+
+echo "Type name of the file extension:"
+read -r extension
+
+file_names=($(find_files_with_extension "$dir_to_search" "$extension"))
+for file_name in "${file_names[@]}"; do
+    echo "$file_name"
+done
+
+exit 0
