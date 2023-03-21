24392fa2ebfdc50cc34ac3760efa8c4907325e8f
Amit <90318118+aamit107@users.noreply.github.com>
2023-03-21 21:11:47 +0600

    Update create_files_with_extensions.sh
    added documentation

--- batch script/create_files_with_extensions.sh
+++ batch script/create_files_with_extensions.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This function creates files with specified extensions in a specified directory
+# Arguments:
+# 1. The name of the directory to create the files in
+# 2. An array of file extensions to use
 function create_files_with_extensions() {
     local dir_to_create="$1"
     local file_extensions=("${@:2}") # Use all the arguments after the first as the file extensions
@@ -8,6 +12,10 @@ function create_files_with_extensions() {
         for ((j = 0; j < ${#file_extensions[@]}; j++)); do
             file_name="$dir_to_create/$i${file_extensions[$j]}"
             output=$(touch "$file_name")
+            
+            # In the script, we are capturing the output of the touch command in the $output variable. If the $output variable has a non-zero 
+            # length, it means that the touch command has encountered an error while creating the file. In this case, the script prints the 
+            # error message using the echo command.
             if [ ${#output} -ne 0 ]; then
                 echo "$output"
             else
